6867781d13afb1f813a95c34910a8b3eb62146b1
289420843 <289420843@qq.com>
2015-12-18 16:34:19 +0800

    提交

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-﻿# BulkModifier
-批量修改器
-# 说明
-1.支持正则表达式检索 <br/>
-2.批量替换自定义脚本
+BulkModifier  批量修改器
+===================================
+  
+  一个简单的批量文本修改工具
+
+  
+特点
+-----------------------------------
+  1.  支持正则表达式查询
+  2.  替换自定义内容
+  
+        
+### 脚本  
+    xxxxxxxx
+    xxxxxxxxxxxxxxxx
+    xxx
+
+### 测试
+    public class HelloWorld {
+      /**
+      * @param args
+      */ 
+      public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+      }
+    }
+
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace UnitTest
 {
@@ -9,6 +12,35 @@ namespace UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            string lineStr = "<book> (R.string.a) = android.R.string.b , R.string.c;R.id.d;";
+            string keyStr = "getResDrawableID(*)";
+            var priceRegex = new Regex(@"(R\.string)");
+            if (priceRegex.IsMatch(lineStr))
+            {
+                Match m = priceRegex.Match(lineStr);
+                for (var i = 0; i < m.Groups.Count;i++ )
+                {
+                    string a = m.Groups[i].Value;
+                    Console.WriteLine(a);
+                }
+            }
+
+            Console.ReadKey();
+
+            //文件根目录
+            string rootDirectory = @"E:\库\APICloud\APICloudSDK";
+            //处理文件的类型
+            string fileExtension = ".xml,.java";
+            //替换的目标
+            string searchContent = "UZResourcesIDFinder.getResDrawableID(";
+            string extractContent = "";
+            string replace = "UZResourcesIDFinder";
+            IList<string> excFiles = FileHelper.filterExtension(rootDirectory, fileExtension);
+            foreach (string filePath in excFiles)
+            {
+                FileHelper.matching(filePath, searchContent);
+            }
+            int fileCount = excFiles.Count;
         }
     }
 }
